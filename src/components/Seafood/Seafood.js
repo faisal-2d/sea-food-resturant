aea0ceca7d20007d15e593931d8598afd1702fbb
faisal-2d <abdullah71faisal@gmail.com>
2022-03-26 00:44:01 +0300

    first commit

---
+++ src/components/Seafood/Seafood.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Seafood = () => {
+    return (
+        <div>
+            <h3>im a sea food</h3>
+        </div>
+    );
+};
+
+export default Seafood;
---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Seafood from '../Seafood/Seafood';
+
+const Shop = () => {
+    return (
+        <div>
+            <h3>Hello</h3>
+            <Seafood></Seafood>
+        </div>
+    );
+};
+
+export default Shop;
