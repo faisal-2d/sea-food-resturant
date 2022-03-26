6a9bf40236136a8e66661fb2da7db3690ae5f180
faisal-2d <abdullah71faisal@gmail.com>
2022-03-26 18:02:10 +0300

    question 2 added in table

--- src/components/Answers/Answers.js
+++ src/components/Answers/Answers.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import './Answers.css'
 
 const Answers = () => {
     return (
-        <div>
-            <p>im in asn compo</p>
+        <div>            
+            <div className='question-container'>
+                <h1>How React Work?</h1>
+                <p>React works in declarative code.</p>
+            </div>  
+            <div className='question-container'>
+            <h1>What is difference between props and state?</h1>
+                <table>
+                    <tr>
+                        <th>props</th><th>state</th>
+                    </tr>
+                    
+                    <tr>
+                        <td>props are read-only. </td><td>state can be changed and changes asynchronously.</td>
+                    </tr>
+                    <tr>
+                        <td>Props can pass data from one component to other components as an argument.</td><td>State stores information about the components.</td>
+                    </tr>
+                    <tr>
+                        <td>Props can be accessed by the child component.</td><td>State cannot be accessed by child components.</td>
+                    </tr>
+                    <tr>
+                        <td>Props make components reusable.</td><td>State cannot make components reusable.</td>
+                    </tr>
+                    
+                </table>
+            </div>          
         </div>
     );
 };
